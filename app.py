cfef56a9fee3092438538f2ffdcf55bb88d1d021
sharonibejih <sharonibejih@gmail.com>
2022-09-20 09:53:51 +0100

    fixed sudo not working on streamlit share

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import time
 import speech_recognition as sr
 from transcribe import transcribe
 
-os.system("sudo apt-get install portaudio19-dev python-pyaudio")
-os.system("pip install pyaudio")
+# os.system("sudo apt-get install portaudio19-dev python-pyaudio")
+# os.system("pip install pyaudio")
+
+os.system("python - m pip install pyaudio")
 
 r = sr.Recognizer()
 
