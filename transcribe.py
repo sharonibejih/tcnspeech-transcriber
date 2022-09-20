2cf9afea099bc7fad1db6f653425fae9c398152a
sharonibejih <sharonibejih@gmail.com>
2022-09-20 13:03:57 +0100

    -

--- transcribe.py
+++ transcribe.py
@@ -4,6 +4,7 @@ import speech_recognition as sr
 import pyttsx3
 import io
 from pydub import AudioSegment
+import streamlit as st
 
 ## load model and processor from the huggingface hub repository.
 processor = Wav2Vec2Processor.from_pretrained("sharonibejih/wav2vec2-large-xlsr-ng-en-sermon")
@@ -18,34 +19,37 @@ def transcribe():
     NOTE:
     This is an endless loop that only stops when the application is stopped.
     """
-    sr.Microphone()
-    st.write(
-        f"MICs Found on this computer: \n {sr.Microphone.list_microphone_names()}")
+    for index, name in enumerate(sr.Microphone.list_microphone_names()):
+        print(f"Microphones index {index}: {name}")
+        st.write(f"Microphones index {index}: {name}")
+    # sr.Microphone(device_index=0)
+    # print(
+    #     f"MICs Found on this computer: \n {sr.Microphone.list_microphone_names()}")
     # Creating a recognition object
-    r = sr.Recognizer()
-    r.energy_threshold = 4000
-    r.dynamic_energy_threshold = False
+    # r = sr.Recognizer()
+    # r.energy_threshold = 4000
+    # r.dynamic_energy_threshold = False
 
-    with sr.Microphone(sample_rate=16000) as source:
-        print("You can start speaking now...")
-        while True:
-            r.adjust_for_ambient_noise(source, 1)
-            audio = r.listen(source)  # pyaudio object
-            # print("You said something now")
-            data = io.BytesIO(audio.get_wav_data())
-            # convert data to numpy array
-            clip = AudioSegment.from_file(data)
-            # convert clip to tensor
-            x = torch.FloatTensor(clip.get_array_of_samples())
+    # with sr.Microphone(sample_rate=16000) as source:
+    #     print("You can start speaking now...")
+    #     while True:
+    #         r.adjust_for_ambient_noise(source, 1)
+    #         audio = r.listen(source)  # pyaudio object
+    #         # print("You said something now")
+    #         data = io.BytesIO(audio.get_wav_data())
+    #         # convert data to numpy array
+    #         clip = AudioSegment.from_file(data)
+    #         # convert clip to tensor
+    #         x = torch.FloatTensor(clip.get_array_of_samples())
 
-            inputs = processor(x, sampling_rate=16000, 
-            return_tensors="pt", padding=True).input_values
+    #         inputs = processor(x, sampling_rate=16000, 
+    #         return_tensors="pt", padding=True).input_values
 
-            logits = model(inputs).logits
+    #         logits = model(inputs).logits
 
-            tokens = torch.argmax(logits, dim=-1)
-            text = processor.batch_decode(tokens)
-            # print("You said", str(text))
-            return text
+    #         tokens = torch.argmax(logits, dim=-1)
+    #         text = processor.batch_decode(tokens)
+    #         # print("You said", str(text))
+    #         return text
             
         
